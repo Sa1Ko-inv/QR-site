8319d0f684dbe8a1847ffb35988375438460ef5e
Dmitriy Blinow <dmitriyblinow@mail.ru>
2025-02-27 23:56:53 +0300

    started the client part, namely configured the react application

---
+++ client/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as style from './App.module.scss'
+
+const App = () => {
+    return (
+        <div>
+            <button className={style.button}>QWE</button>
+        </div>
+    )
+}
+
+export default App;
